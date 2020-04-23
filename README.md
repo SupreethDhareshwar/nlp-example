149fdcb569b02095f969e7584ff7ee822689e03e
Supreeth Dhareshwar <supreeth.dhareshwar@gmail.com>
2020-04-23 20:48:19 +0530

    Update to plot 50 most common words

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ## Pre-requisuites
 
 * This project uses python 3.8 running on windows 10
-* Run `py -m pip install <package_name>` for installing packages like nltk and BeautifulSoup
-Example : `py -m pip install nltk` and `py -m pip install beautifulsoup4`
+* Run `py -m pip install <package_name>` for installing packages like nltk, BeautifulSoup and matplotlib
+Example : `py -m pip install nltk` and `py -m pip install beautifulsoup4` `py -m pip install matplotlib`
 * For nltk, if you get punkt error, run on python shell the following :
     `import nltk
     nltk.download('punkt')`
--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from nltk import word_tokenize
 from nltk.util import ngrams
 from nltk.stem import PorterStemmer 
 from collections import Counter
+from nltk.probability import FreqDist
 
 ps = PorterStemmer() 
 
@@ -45,7 +46,34 @@ def corpusAnalysis():
         trigramsStem = ngrams(stemToken, 3)
         unigramStemFrequencies += Counter(unigramsStem)
         bigramStemFrequencies += Counter(bigramsStem)
-        trigramStemFrequencies += Counter(trigramsStem)    
+        trigramStemFrequencies += Counter(trigramsStem)
+
+    #Before Stemming
+    print("Total unique unigrams : ",len(unigramFrequencies))
+    freqDistUni = FreqDist(unigramFrequencies)  
+    freqDistUni.plot(50,title="Plot of the 50 most common 1-grams")   
+
+    print("Total unique bigrams : ",len(bigramFrequencies))
+    freqDistBi = FreqDist(bigramFrequencies)  
+    freqDistBi.plot(50,title="Plot of the 50 most common 2-grams")   
+
+    print("Total unique trigrams : ",len(trigramFrequencies))
+    freqDistTri = FreqDist(trigramFrequencies)  
+    freqDistTri.plot(50,title="Plot of the 50 most common 3-grams")   
+
+    #After Stemming
+    print("Total unique unigrams after stemming : ",len(unigramStemFrequencies))
+    freqDistUniStem = FreqDist(unigramStemFrequencies)  
+    freqDistUniStem.plot(50,title="Plot of the 50 most common 1-grams after stemming")   
+    
+    print("Total unique bigrams after stemming : ",len(bigramStemFrequencies))
+    freqDistBiStem = FreqDist(bigramStemFrequencies)  
+    freqDistBiStem.plot(50,title="Plot of the 50 most common 2-grams after stemming")   
+    
+    print("Total unique trigrams after stemming : ",len(trigramStemFrequencies)) 
+    freqDistTriStem = FreqDist(trigramStemFrequencies)  
+    freqDistTriStem.plot(50,title="Plot of the 50 most common 3-grams after stemming")   
+    
     return
 
 #Start of Code
